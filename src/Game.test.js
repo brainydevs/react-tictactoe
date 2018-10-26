acc52b9988cc47105f9d7c952056f6df91865f65
Brainydevs <arnold@brainydevs.com>
2018-10-26 18:49:33 +0200

    Added small test.

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Game from './Game';
+import Board from './Board';
 
 it('renders without crashing', () => {
   shallow(<Game />);
+});
+
+it('contains a board', () => {
+  let wrapper = shallow(<Game />);
+  expect(wrapper.find('Board').length).toBe(1);
 });
