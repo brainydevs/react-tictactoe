a130524c5f1006c17d02d7353e90dd910131642a
Brainydevs <arnold@brainydevs.com>
2018-10-26 18:08:09 +0200

    Added enzyme and setupTests file

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import Game from './Game';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<Game />);
 });
